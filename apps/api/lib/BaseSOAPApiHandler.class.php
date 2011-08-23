d0e267498d56fab5a3835514461803614cd06d59
unknown <shadley@.eset.co.za>
2011-08-23 13:28:11 +0200

    Added soap classes

--- apps/api/lib/BaseSOAPApiHandler.class.php
+++ apps/api/lib/BaseSOAPApiHandler.class.php
@@ -5,6 +5,11 @@
  */
 class BaseSOAPApiHandler extends ckSoapHandler
 {
+  public function CreateClient($token)
+  {
+    return sfContext::getInstance()->getController()->invokeSoapEnabledAction('client', 'create', array($token));
+  }
+
   public function ListClients($token)
   {
     return sfContext::getInstance()->getController()->invokeSoapEnabledAction('client', 'list', array($token));
@@ -13,5 +18,10 @@ class BaseSOAPApiHandler extends ckSoapHandler
   public function Authenticate($token, $username, $password)
   {
     return sfContext::getInstance()->getController()->invokeSoapEnabledAction('login', 'authenticate', array($token, $username, $password));
+  }
+
+  public function CreateQuote($token)
+  {
+    return sfContext::getInstance()->getController()->invokeSoapEnabledAction('quote', 'create', array($token));
   }
 }
