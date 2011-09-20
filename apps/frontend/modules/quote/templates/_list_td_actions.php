ed811b8c0f0f40eac8d3f417dedbe83eaec7ec02
Shadley Wentzel <shad6ster@gmai.com>
2011-09-20 11:47:31 +0200

    Removed uneeded file

--- apps/frontend/modules/quote/templates/_list_td_actions.php
+++
@@ -1,9 +0,0 @@
-<td>
-  <ul class="sf_admin_td_actions">
-    <li class="sf_admin_action_generate">
-      <?php echo link_to(__('View', array(), 'messages'), url_for('quote_list', $quote), array()) ?>      
-    </li>  
-    <?php echo $helper->linkToRefresh($quote, array(  'params' =>   array(  ),  'class_suffix' => 'refresh',  'label' => 'Refresh',)) ?>    
-    <?php echo $helper->linkToDelete($quote, array(  'params' =>   array(  ),  'confirm' => 'Are you sure?',  'class_suffix' => 'delete',  'label' => 'Delete',)) ?>
-  </ul>
-</td>
